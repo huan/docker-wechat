c18d79faefe99210c7612afedf6bb7f8ffb8ae57
Huan (李卓桓) <zixia@zixia.net>
2020-02-17 12:43:11 +0000

    add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+if [ -n "$AUDIO_GID" ]; then
+  groupmod -o -g $AUDIO_GID audio
+fi
+if [ -n "$VIDEO_GID" ]; then 
+  groupmod -o -g $VIDEO_GID video
+fi
+if [ $GID != $(echo `id -g user`) ]; then
+    groupmod -o -g $GID group
+fi
+if [ $UID != $(echo `id -u user`) ]; then
+    usermod -o -u $UID user
+fi
+
+# FileSavePath
+chown user:group /WechatFiles
+
+# wine reg DELETE 'HKCU\Software\Tencent\WeChat' UpdateFailCnt /f &> /dev/null
+# wine reg DELETE 'HKCU\Software\Tencent\WeChat' NeedUpdateType /f &> /dev/null
+# rm "${WINEPREFIX}/drive_c/users/${USER}/Application Data/Tencent/WeChat/All Users/config/configEx.ini"
+
+su user -c "wine reg QUERY 'HKEY_CURRENT_USER\Software\Tencent\WeChat'"
+su user -c "wine 'C:\Program Files\Tencent\WeChat\WeChat.exe'"
