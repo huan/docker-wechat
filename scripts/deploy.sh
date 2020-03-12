e1a8d62d93c6f7a5384c022680616979da11d489
Huan LI <zixia@zixia.net>
2020-03-12 13:58:39 +0800

    use a deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+function main () {
+  ARTIFACT_IMAGE=$1
+
+  IMAGE=$(cat IMAGE)
+  TAG=$(cat VERSION)
+
+  # https://stackoverflow.com/a/58453200/1123955
+  WECHAT_VERSION=$(
+    docker run -it --rm \
+    -a stdout \
+    --entrypoint cat \
+    "$ARTIFACT_IMAGE" \
+    /home/VERSION.WeChat
+  )
+
+  echo "Deploying IMAGE=$IMAGE"
+  docker tag "${ARTIFACT_IMAGE}" "${IMAGE}"  # latest
+  docker push "${IMAGE}"
+
+  echo "Deploying TAG=$TAG"
+  docker tag "${ARTIFACT_IMAGE}" "${IMAGE}:${TAG}"
+  docker push "${IMAGE}:${TAG}"
+
+  echo "Deploying WECHAT_VERSION=$WECHAT_VERSION"
+  docker tag "${ARTIFACT_IMAGE}" "${IMAGE}:${WECHAT_VERSION}"
+  docker push "${IMAGE}:${WECHAT_VERSION}"
+}
+
+if [ -z "$1" ]; then
+  >&2 echo "Missing argument: ARTIFACT_IMAGE"
+  exit 1
+fi
+
+main $1
