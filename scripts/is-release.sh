1810188f84fa4133b5d784c637c5b4b7d2fc290c
Huan LI <zixia@zixia.net>
2020-03-12 15:28:00 +0800

    lint

--- scripts/is-release.sh
+++ scripts/is-release.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
 # 1.2.3
 # 2.3.4
 function isRelease () {
-  minor=$(echo $1 | cut -d. -f2)
+  minor=$(echo "$1" | cut -d. -f2)
   mod2=$((minor%2))
 
   # echo "isRelease: $1 minor $minor release $release"
-  return "$mod2"
+  return $mod2
 }
