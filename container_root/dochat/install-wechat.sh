7cc6367d42a668f0120e6d9ff073316483f1756a
Huan LI (李卓桓) <zixia@zixia.net>
2020-04-27 01:28:04 +0800

    code clean

--- container_root/dochat/install-wechat.sh
+++ container_root/dochat/install-wechat.sh
@@ -8,5 +8,6 @@ set -eo pipefail
 # HOME_TGZ_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.7.1.88.tgz
 # HOME_TGZ_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.8.0.121.tgz
 HOME_TGZ_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.9.0.114.tgz
+
 curl -sL "$HOME_TGZ_URL" | tar zxf -
 echo 'Artifacts Downloaded'
