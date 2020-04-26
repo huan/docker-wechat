52d771b8d11b8e099b23986e6072d2055a8415c6
Huan LI (李卓桓) <zixia@zixia.net>
2020-04-26 14:27:49 +0800

    Build 2.7.1.85

--- container_root/setup.sh
+++ container_root/setup.sh
@@ -4,9 +4,10 @@ set -eo pipefail
 #
 # Download WeChat Installed Files
 #
-HOME_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.9.0.114.tgz
-# ARG HOME_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.8.0.121.tgz
-# ARG HOME_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.7.1.88.tgz
+# HOME_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.9.0.114.tgz
+# HOME_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.8.0.121.tgz
+# HOME_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.7.1.88.tgz
+HOME_URL=https://github.com/huan/docker-wechat/releases/download/v0.1/home.2.7.1.85.tgz
 curl -sL "$HOME_URL" | tar zxf -
 echo 'Artifacts Downloaded'
 
