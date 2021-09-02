0c014d51e7605672b496f80ed063ff3f0c3bfe74
Huan <zixia@zixia.net>
2021-09-02 19:57:12 +0800

    fix peres unexpected DEBUG message (https://github.com/merces/pev/issues/158)

--- container_root/dochat/peres-wechat-version.sh
+++ container_root/dochat/peres-wechat-version.sh
@@ -4,7 +4,12 @@ set -eo pipefail
 #
 # Generate WeChat Version file by `peres` tool
 #
-WECHAT_DIR='/home/user/.wine/drive_c/Program Files/Tencent/WeChat'
-
-peres -v "$WECHAT_DIR"/WeChatWin.dll | awk '{print $3}' > /home/VERSION.WeChat
-echo 'WeChat VERSION generated'
+e
+#
+# Generate WeChat Version file
+#   Product Version:                 3.3.0.115
+#     -> 3.3.0.115
+#
+peres -v "$WECHAT_DIR"/WeChatWin.dll | grep 'Product Version: ' | awk '{print $3}' > /home/VERSION.WeChat
+echo 'WeChat VERSION generated:'
+cat /home/VERSION.WeChat
